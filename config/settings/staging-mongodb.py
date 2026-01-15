f3692596e4099bc7ad3a090130e9ef9d809f5bc6
lifanwar <mualifulanwar1@gmail.com>
2026-01-15 03:34:23 +0200

    implementasi dengan datbase mongoDb

---
+++ config/settings/staging-mongodb.py
@@ -0,0 +1,33 @@
+from ..base import *
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+DEBUG = False
+ALLOWED_HOSTS = ['dkkm.jastipin.id']
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',  # Bukan postgresql!
+        'NAME': os.getenv('STAGGING_DB_NAME'),
+        'CLIENT': {
+            'host': f"mongodb://{os.getenv('STAGGING_DB_USER')}:{os.getenv('STAGGING_DB_PASSWORD')}@{os.getenv('STAGGING_DB_HOST')}:{os.getenv('STAGGING_DB_PORT')}/{os.getenv('STAGGING_DB_NAME')}"
+        }
+    }
+}
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://dkkm.jastipin.id',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+SECURE_SSL_REDIRECT = False
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Mengaktifkan HSTS dengan waktu tertentu (misalnya 31536000 detik = 1 tahun)
+SECURE_HSTS_SECONDS = 31536000  # 1 tahun
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Jika ingin mengaktifkan HSTS untuk subdomain
+SECURE_HSTS_PRELOAD = True  # Untuk mendaftarkan domain di preload list HSTS
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
