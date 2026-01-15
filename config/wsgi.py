5d2254080a1d1abcc7ea29571ea7ff294c31cff7
lifanwar <mualifulanwar1@gmail.com>
2026-01-15 03:54:43 +0200

    menambahkan implementasi staging sqlite

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.staging-mongodb')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.staging-sqlite')
 
 application = get_wsgi_application()
