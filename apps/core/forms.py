2ab07ac6370ff22d711a9890de0bae2aac6d2930
lifanwar <mualifulanwar1@gmail.com>
2026-01-23 21:27:33 +0200

    hapuss yang tidak dibutuhkan

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 import re
-from django.utils.safestring import mark_safe
 from .models import Gedung
 from apps.core.widgets import LeafletCoordinatesWidget
 
